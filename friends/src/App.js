0dd5194a74ca70dc243b6c244a0ebd9c74feacf8
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-28 17:00:57 -0500

    style app and index

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
 
-import './App.css';
 import FriendList from './components/FriendList';
 import FriendForm from './components/FriendForm';
+import Reset from './components/Reset';
+import { Button } from './components/Common';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 1.8rem;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AppTitle = styled.h1`
+  font-size: 8.5rem;
+`;
 
 class App extends Component {
   state = {
@@ -42,8 +72,10 @@ class App extends Component {
   render() {
     const { friends } = this.state;
     return (
-      <div className="App">
-        <h1>Friends</h1>
+      <AppWrapper>
+        <Reset />
+        <GlobalStyle />
+        <AppTitle>Friends</AppTitle>
         <FriendList
           friends={friends}
           editFriend={this.editFriend}
@@ -53,16 +85,22 @@ class App extends Component {
           exact
           path="/"
           render={props => (
-            <button onClick={() => props.history.push('/friends/new')}>
+            <Button primary onClick={() => props.history.push('/friends/new')}>
               Add Friend
-            </button>
+            </Button>
           )}
         />
         <Route
           path="/friends/new"
-          render={props => <FriendForm {...props} addFriend={this.addFriend} />}
+          render={props => (
+            <FriendForm
+              {...props}
+              addFriend={this.addFriend}
+              cancel={() => props.history.push('/')}
+            />
+          )}
         />
-      </div>
+      </AppWrapper>
     );
   }
 }
